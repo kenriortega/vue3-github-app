e969c7bbf7a7426bcfde958eeefd4f11994c8030
kenriortega <kenriortega@gmail.com>
2020-09-18 17:04:04 -0400

    [REFACTOR]

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Github API</h1>
+    <div>
+      <input v-model="name" type="text" />
+      <button @click="newName = name">Click Here</button>
+    </div>
+    <div>
+      <ul>
+        <li v-for="repo in repos" :key="repo.id">{{ repo.name }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { reactive, ref, toRefs, watch } from 'vue'
+  export default {
+    name: 'App',
+    setup() {
+      const name = ref(null)
+      const newName = ref(null)
+      const state = reactive({ repos: [] })
+      watch(async () => {
+        if (newName.value) {
+          let res = await fetch(
+            `https://api.github.com/users/${newName.value}/repos`
+          )
+          let data = await res.json()
+          state.repos = data
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+          name.value = ''
+        }
+      })
+      return {
+        name,
+        newName,
+        ...toRefs(state)
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
